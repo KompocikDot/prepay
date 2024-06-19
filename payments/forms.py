43e6f3c3090b42d984701840079a2bc993e4036b
Kacper Soczko <kacper.soczko@gmail.com>
2024-06-19 20:32:35 +0200

    Style create payment page

--- payments/forms.py
+++ payments/forms.py
@@ -8,18 +8,18 @@ class CreatePaymentForm(ModelForm):
         super().__init__(*args, **kwargs)
         self.fields["amount"].widget.widgets[0].attrs.update(
             {
-                "class": "block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                "class": "block w-full rounded-l-md border-0 py-1.5 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             }
         )
 
         self.fields["amount"].widget.widgets[1].attrs.update(
             {
-                "class": "h-full rounded-md border-0 bg-transparent py-0 pl-2 pr-7 text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                "class": "block rounded-r-md border-0 py-1.5 pl-2 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             }
         )
         self.fields["name"].widget.attrs.update(
             {
-                "class": "block w-full rounded-md border-0 py-1.5 pl-7 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                "class": "block w-full rounded-md border-0 py-1.5 pl-2 pr-20 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             }
         )
 
