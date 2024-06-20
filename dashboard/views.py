7b38853e93fb72e00f434f7136f4f5236aafdd92
Kacper Soczko <kacper.soczko@gmail.com>
2024-06-20 17:20:44 +0200

    Few visual fixes

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,14 +1,17 @@
+import logging
 from typing import Any
+
+import stripe
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
 
 from payments.models import Payment
 
 
-class DashboardView(TemplateView):
+class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = "./dashboard/dashboard.html"
 
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
-
         user = self.request.user
         issued_payments_count = Payment.objects.filter(issuer=user).count()
         last_five_end_user_payments = Payment.objects.filter(end_user=user).order_by(
@@ -23,4 +26,12 @@ class DashboardView(TemplateView):
             "end_user_payments_count": end_user_payments_count,
         }
 
+        self.get_stripe_balance()
         return ctx
+
+    def get_stripe_balance(self):
+        balance = stripe.Balance.retrieve(
+            stripe_account=self.request.user.stripe_account_id
+        )
+
+        logging.error(balance)
